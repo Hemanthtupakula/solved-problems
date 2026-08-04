0e780b5a810b8d1e07a72108efbc3832b2a41cb7
Your Name <you@example.com>
2026-08-04 15:10:55 +0530

    Convert Payment abstract class to an interface and implement it

--- Abs.java
+++ Abs.java
@@ -1,47 +1,93 @@
-abstract class Payment {
+interface Payment {
+    void display();
+    void pay();
+    String getName();
+    int getAmt();
+    void setAmt(int amt);
+}
+
+class RockPayments implements Payment {
     private String name;
     private int amt;
 
-    public Payment(String name, int amt) {
+    public RockPayments(String name, int amt) {
         this.name = name;
         this.amt = amt;
     }
 
+    @Override
     public void display() {
         System.out.println(this.name);
         System.out.println(this.amt);
-    }
-
-    public abstract void pay();
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getAmt() {
-        return amt;
-    }
-
-    public void setAmt(int amt) {
-        this.amt = amt;
-    }
-}
-
-class RockPayments extends Payment {
-    public RockPayments(String name, int amt) {
-        super(name, amt);
     }
 
     @Override
     public void pay() {
         System.out.println("rock payments");
     }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAmt() {
+        return this.amt;
+    }
+
+    @Override
+    public void setAmt(int amt) {
+        this.amt = amt;
+    }
+}
+
+class CardPayments implements Payment {
+    private String name;
+    private int amt;
+
+    public CardPayments(String name, int amt) {
+        this.name = name;
+        this.amt = amt;
+    }
+
+    @Override
+    public void display() {
+        System.out.println(this.name);
+        System.out.println(this.amt);
+    }
+
+    @Override
+    public void pay() {
+        System.out.println("card payments");
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAmt() {
+        return this.amt;
+    }
+
+    @Override
+    public void setAmt(int amt) {
+        this.amt = amt;
+    }
 }
 
 public class Abs {
     public static void main(String[] args) {
-        Payment p = new RockPayments("hemanth", 1000);
-        p.display();
-        p.pay();
+        Payment p1 = new RockPayments("hemanth", 1000);
+        p1.display();
+        p1.pay();
+
+        System.out.println();
+
+        Payment p2 = new CardPayments("tupakula", 2500);
+        p2.display();
+        p2.pay();
     }
 }
